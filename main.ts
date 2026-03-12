08017957e1f91a2fa814a0c39a9c09cca33eb934
Brandon-Tsang <brandon.tsang@stu.ocsb.ca>
2026-03-12 13:27:29 -0400

    Tells distance using the sonar

--- main.ts
+++ main.ts
@@ -5,4 +5,26 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+let distanceToObject: number = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+// locate distance from sonar
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    
+    // get distance to object
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+
+    // show the distance
+    basic.clearScreen()
+    basic.showString((distanceToObject) + ' cm.')
+
+    // reset
+    basic.showIcon(IconNames.Happy)
+})
